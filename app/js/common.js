67ca3552b9a99e0388ffa7645f263aaf429a7bb4
viktoriazaharova <zaharova181114@gmail.com>
2020-11-02 23:56:38 +0200

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -42,3 +42,11 @@ $('.reasons-slider').slick({
     infinite: false,
     appendDots: '.reasons-slider__nav'
 });
+
+$('.about-page__slider').slick({
+    slidesToShow: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    fade: true
+});
