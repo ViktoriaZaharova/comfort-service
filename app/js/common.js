7aa202e3ea4c5643af2c11bd80d46e098dea8a25
viktoriazaharova <zaharova181114@gmail.com>
2020-11-03 02:57:49 +0200

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -50,3 +50,29 @@ $('.about-page__slider').slick({
     nextArrow: '<button type="button" class="slick-next"></button>',
     fade: true
 });
+
+$('.reviews-wrapper__slider').slick({
+    slidesToShow: 4,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
